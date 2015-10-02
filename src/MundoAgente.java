7b85134912fc34c788c1d065bedef815c8c4e21c
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2015-10-01 21:49:26 -0300

    Remoção de arquivos antigos.

--- src/MundoAgente.java
+++
@@ -1,8 +0,0 @@
-/**
- *
- * A classe MundoAgente determina uma interface padrão para as classes de exibição
- * do ambiente do agente.
- */
-public interface MundoAgente {
-    public void atualizaCiclo(Ambiente ambiente);
-}
